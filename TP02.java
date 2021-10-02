ca9d7ff2265e6f790067c14602793a32a531acc9
Haidar341 <91077398+Haidar341@users.noreply.github.com>
2021-10-02 21:36:38 +0700

    TP02.java

---
+++ TP02.java
@@ -0,0 +1,11 @@
+// mengimpor Scanner ke program
+import java.util.Scanner;
+public class TP02 {
+    public static void main(String[] args) {
+	// Menampilkan apa yang sudah simpan di variabel	
+         String nama = "Haidar JIndan Ahnif";
+         String Umur = "18 tahun ya!";
+         System.out.println("Hallo," + nama);
+		 System.out.println("Ternyata usiamu baru" + Umur);
+	}	 
+}	 
